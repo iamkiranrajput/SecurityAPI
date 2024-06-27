539c5e113e37f2e300737e72ece69f4d995e4551
iamkiranrajput <rajputkiran2805@gmail.com>
2024-06-27 21:39:21 +0530

    basic security

--- src/main/java/com/security/config/SpringSecurity.java
+++ src/main/java/com/security/config/SpringSecurity.java
@@ -4,19 +4,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 
-@Configuration
-@EnableWebSecurity
+@Configuration		//this annotation this calls provide configuration of application 
+@EnableWebSecurity  //to enable web security features in the application 
 public class SpringSecurity{
 	@Bean
 	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 		
-		http.authorizeHttpRequests((request)->request.anyRequest().authenticated());
+		http.authorizeHttpRequests((request)->request.anyRequest().authenticated());  
+		//any request will be authenticated 
+		http.sessionManagement(session
+		->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 		
-		http.formLogin(); 
-//		http.httpBasic(); 
+//		http.formLogin(); 
+		http.httpBasic();   // basic authentication with default settings 
 		return http.build();
 		
 	}
