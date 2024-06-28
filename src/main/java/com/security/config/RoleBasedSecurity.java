206f068d0c328053b89bc8c41820c530e0d9f48c
iamkiranrajput <rajputkiran2805@gmail.com>
2024-06-28 15:56:01 +0530

    Role based Security

---
+++ src/main/java/com/security/config/RoleBasedSecurity.java
@@ -0,0 +1,45 @@
+package com.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration		//this annotation this calls provide configuration of application
+@EnableWebSecurity  //to enable web security features in the application
+@EnableMethodSecurity
+public class RoleBasedSecurity{
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((request)->request.anyRequest().authenticated());
+        //any request will be authenticated
+        http.sessionManagement(session
+                ->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+//		http.formLogin();
+        http.httpBasic();   // basic authentication with default settings
+        return http.build();
+
+    }
+
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user1= User.withUsername("user").password("{noop}user").roles("USER").build();
+
+        UserDetails admin=User.withUsername("admin").password("{noop}admin").roles("ADMIN").build();
+        UserDetails admin2=User.withUsername("admin2").password("{noop}admin2").roles("ADMIN").build();
+        return new InMemoryUserDetailsManager(user1,admin,admin2);
+    }
+
+
+}
--- src/main/java/com/security/controller/Controller.java
+++ src/main/java/com/security/controller/Controller.java
@@ -1,5 +1,6 @@
 package com.security.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +12,17 @@ public class Controller {
 	@GetMapping("/")
 	public static String welcome() {
 		return "welcome";
+	}
+
+//	@PreAuthorize("hasRole('USER')")
+	@GetMapping("user")
+	public String userEndPoint(){
+		return "hello User";
+	}
+
+	@PreAuthorize("hasRole('ADMIN')")
+	@GetMapping("admin")
+	public String adminEndPoint(){
+		return "hello Admin";
 	}
 }
